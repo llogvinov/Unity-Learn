84a66c317393c869aa4feb75fb4681b72e6cfa0f
Lev Logvinov <llogvinov356@gmail.com>
2021-06-03 12:28:39 +0300

    prototype 2 added

---
+++ Prototype 2/Assets/Scripts/DestroyOutOfBound.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOutOfBound : MonoBehaviour
+{
+    private float topBound = 30.0f;
+    private float lowerBound = -10.0f;
+
+    void Update()
+    {
+        if (transform.position.z > topBound)
+        {
+            gameObject.SetActive(false);
+        } else if (transform.position.z < lowerBound)
+        {
+            Debug.Log("Game Over");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float spawnPosX = 10.0f;
+    private float spawnPosZ = 20.0f;
+
+    private float startDelay = 2.0f;
+    private float spawnInterval = 1.5f;
+
+    private void Start()
+    {
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+    }
+
+    void SpawnRandomAnimal()
+    {
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnPosX, spawnPosX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+}
