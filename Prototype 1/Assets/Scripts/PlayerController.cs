0512182c6998e69b7be0bf7b5f16f533ff8aa675
Lev Logvinov <llogvinov356@gmail.com>
2021-06-03 12:27:39 +0300

    prototype 1 added

---
+++ Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float horsePower = 0;
+
+    private Rigidbody playerRb;
+    [SerializeField] GameObject centerOfMass;
+
+    public float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+
+        // Move vehicle forward 
+        playerRb.AddRelativeForce(Vector3.forward * forwardInput * horsePower);
+
+        // Rotate vehicle
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+    }
+}
